388f0f5518ce14be8817f81a1e27c4c49eedfcb6
Laurentiu <laurentiulaurentiu04@gmail.com>
2023-07-03 10:04:41 +0300

    added functionality to the taskbar buttons

--- src/js-files/home.js
+++ src/js-files/home.js
@@ -13,9 +13,10 @@ export default function Home() {
         <>
             <div id="home">
                 <Draggable
-                handle=".bar">
+                handle=".win_handle">
                     <div className="window image">
                         <div className="bar">
+                            <div className="win_handle"></div>
                             <p><b>Laurențiu</b>_Laslău.png</p>
                             <div style={{display: "inline-flex", width: "min-content"}}>
                             <button className="hide" onClick={handleButton}>_</button>
@@ -28,10 +29,11 @@ export default function Home() {
                     </div>
                 </Draggable>
                 <Draggable
-                handle=".bar">
+                handle=".win_handle">
                     <div className="window description">
                         <div className="bar">
-                            <p>my_<b>description</b>.png</p>
+                            <div className="win_handle"></div>
+                            <p>my_<b>description</b>.txt</p>
                             <div style={{display: "inline-flex", width: "min-content"}}>
                             <button className="hide" onClick={handleButton}>_</button>
                             <button className="close">x</button>
--- src/js-files/navbar.js
+++ src/js-files/navbar.js
@@ -4,13 +4,22 @@ import pics_icon from "../Assets/svg/pics-icon.svg"
 import files_icon from "../Assets/svg/document-icon.svg"
 
 export default function Navbar() {
+
+    const handleOpen = (evt) => {
+
+        if (evt.target.className === "pic")
+            document.getElementsByClassName("image")[0].className = "window image react-draggable"
+            if (evt.target.className === "document")
+            document.getElementsByClassName("description")[0].className = "window description react-draggable"
+    }
+
     return(
         <>
             <div id="navbar">
                 <img className= "logo"src={logo}/>
                 <div className="opened-apps">
-                    <img src={pics_icon}/>
-                    <img src={files_icon}/>
+                    <img className="pic" src={pics_icon} onClick={handleOpen}/>
+                    <img className="document" onClick={handleOpen} src={files_icon}/>
                 </div>
             </div>
         </>
